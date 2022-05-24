b914c6767a3f4fc47ef38eee009d1e2bd3859a9f
Anna-Zeri Temir <azjuliantemir@gmail.com>
2022-05-24 11:03:14 +0100

    fizz buzz fola test

--- src/skeleton.test.ts
+++ src/skeleton.test.ts
@@ -1,5 +1,17 @@
-import sum from "./skeleton";
+import fizzbuzz from "./skeleton";
 
-test("sum adds two numbers", () => {
-  expect(sum(4, 3)).toBe(7);
+test("should list numbers up to 2", () => {
+  expect(fizzbuzz(2)).toStrictEqual([1, 2]);
+});
+
+test("should list numbers up to 3, replacing 3 with Fizz", () => {
+  expect(fizzbuzz(3)).toStrictEqual([1, 2, "Fizz"]);
+});
+
+test("should list numbers up to 5, replacing 3 with Fizz and 5 with Buzz", () => {
+  expect(fizzbuzz(5)).toStrictEqual([1, 2, "Fizz", 4, "Buzz"]);
+});
+
+test("should list numbers up to 15, replacing 3 with Fizz and 5 with Buzz and 15 with FizzBuzz", () => {
+  expect(fizzbuzz(15)).toStrictEqual([1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz"]);
 });
--- src/skeleton.ts
+++ src/skeleton.ts
@@ -1,11 +1,63 @@
 /**
- * Adds together two numbers
- * @param a - the first number to add
- * @param b - the second number to add
- * @returns the total
+ * 
+ * Return an array containing the numbers from 1 to N, where N is the parametered value.
+
+Replace certain values however if any of the following conditions are met:
+
+- If the value is a multiple of 3: use the value "Fizz" instead
+- If the value is a multiple of 5: use the value "Buzz" instead
+- If the value is a multiple of 3 & 5: use the value "FizzBuzz" instead
+
+N will never be less than 1.
+
+
+
  */
-function sum(a: number, b: number): number {
-  return a + b;
+
+/* PSEUDOCODE
+function 
+parameter: num (number)
+return: array of numbers/ string
+
+initialise empty array called fizzBuzzArray
+initialise a for loop starting from i=1, i++, up to num:
+  if i%3 is equivalent to 0 and if i%5 is equivalent to 0:
+    push "fizzbuzz" into fizzBuzzArray
+  else if i%3 is equivalent to 0:
+    push "fizz" into fizzBuzzArray
+  else if i%5 is equiavalent to 0:
+    push "buzz" into fizzBuzzArray
+  else:
+    push i into fizzBuzzArray
+  
+  END IF
+END FOR
+
+return fizzBuzzArray
+    
+*/ 
+
+
+function fizzbuzz(num: number): (string|number)[] {
+  const fizzBuzzArray: (string|number)[] = [];
+  for (let i=1; i<=num; i++){
+    if ((i%3===0)&&(i%5===0)){
+      fizzBuzzArray.push("FizzBuzz")
+    }
+    else if (i%3===0){
+      fizzBuzzArray.push("Fizz")
+    }
+    else if(i%5===0){
+      fizzBuzzArray.push("Buzz")
+    }
+    else{
+      fizzBuzzArray.push(i)
+    }
+  }
+
+  return fizzBuzzArray;
 }
 
-export default sum;
+export default fizzbuzz;
+
+console.log(fizzbuzz(3))
